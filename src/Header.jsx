983661c1e2bd6a94c6df07a259547835e87a7d46
yasirkhan-4925 <iammuhammadyasirkhan@gmail.com>
2020-09-30 20:32:27 +0500

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+function Header(){
+     
+     return(
+
+          <div>
+          <div className="container-fluid bg-danger">
+
+
+               <h1 className="text-light text-center my-2">Saylani Web and Mobile Course</h1>
+               <h3 className="text-light text-center my-2">Name : Muhammad Yasir Khan</h3>
+               <h2 className="text-light text-center my-2">Roll No: WM-8601</h2>
+
+               <br/> 
+               <br/>
+
+               
+
+
+               
+
+          </div>
+          <h1 className="text-dark text-center my-4" >My Projects</h1>
+          <hr/>
+          </div>
+     )
+     
+}
+
+export default Header
---
+++ src/MyProjects.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function MediaCard(props) {
+  const classes = useStyles();
+  
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.imagelink}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.details}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+         
+        <Button href={props.buttonlink}  size="large" variant="outlined" color="secondary" style={{width:"100%"}} >
+          Check
+        </Button>
+     
+        
+      </CardActions>
+    </Card>
+  );
+}
